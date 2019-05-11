c356cffc352c765049e03d9ab7f9db9044e01330
rtorres <rtorres@pop-os.localdomain>
2019-05-11 19:10:07 +0100

    seqSieve.cpp prepared

--- seqSieve.cpp
+++ seqSieve.cpp
@@ -2,9 +2,10 @@
 #include <iomanip>
 #include <math.h>
 #include <omp.h>
+#include <fstream>
 using namespace std;
 
-void sequentialSieve (unsigned long long n)
+double sequentialSieve (unsigned long long n)
 {
  
     n = pow(2,n);
@@ -12,7 +13,7 @@ void sequentialSieve (unsigned long long n)
     bool *primes = new bool[n];
     
     unsigned long long k = 3;
-    float clock_time = (float)clock();
+    double timeCounter = -(double)clock();
     do
     {
         for (unsigned long long j = k*k ; j<n ; j+=2*k)
@@ -25,23 +26,24 @@ void sequentialSieve (unsigned long long n)
         }while (k*k <= n && primes[k>>1]);
         
     } while (k*k <= n);
-     clock_time = (float)((clock() - clock_time)/CLOCKS_PER_SEC);
 
-    int numberOfPrimes=0;
+    timeCounter += (double)clock();
+    timeCounter /= CLOCKS_PER_SEC;
+    return timeCounter;
+    /*int numberOfPrimes=0;
     for (unsigned long long i=1; i<n; i+=2)
         if (!primes[i>>1])
-            numberOfPrimes++;
+            numberOfPrimes++;*/
 
-    cout << numberOfPrimes << endl;
 }
 
-void sieveOpenMp(unsigned long long n,  int n_threads){
+double sieveOpenMp(unsigned long long n,  int n_threads){
     n = pow(2,n);
     
     bool *primes = new bool[n]; //initialized as false
     
     unsigned long long k = 3;
-    float clock_time = (float)omp_get_wtime();
+    double timeCounter = -(double)omp_get_wtime();
     do
     {
         #pragma omp parallel for num_threads(n_threads)
@@ -56,19 +58,43 @@ void sieveOpenMp(unsigned long long n,  int n_threads){
         
     } while (k*k <= n);
 
-    clock_time = (float)((omp_get_wtime() - clock_time));
-
+    timeCounter += (double)(omp_get_wtime());
+    return timeCounter;
+    /*
     int numberOfPrimes=0;
     for (unsigned long long i=1; i<n; i+=2)
         if (!primes[i>>1])
-            numberOfPrimes++;
+            numberOfPrimes++;*/
 
-    cout << numberOfPrimes << endl;
+    
 }
 
 int main(){
-    sieveOpenMp(25,2);
-    sieveOpenMp(32,2);
+
+    std::ofstream myfile;
+	myfile.open("Benchmark1.csv");
+    myfile << "Algorithm, Run Number, 2^n size, Time, num_threads\n";
+
+    for (int j = 25; j <= 25; j ++ )
+		for (int i = 0; i < 3; i++)
+		{
+			
+			double time = sequentialSieve(j);
+			
+			myfile << "1," << i << "," << j << "," << setprecision(3) <<time <<",\n";
+			cout << "1," << i << "," << j << "," << setprecision(3) <<time <<",\n";
+		}
+    for (int k=2; k<=3;k++)
+        for (int j = 25; j <= 25; j ++ )
+            for (int i = 0; i < 3; i++)
+            {
+                
+                double time = sieveOpenMp(j,k);
+                
+                myfile << "1," << i << "," << j << "," << setprecision(3) <<time <<"," <<k<<",\n";
+                cout << "1," << i << "," << j << "," << setprecision(3) <<time <<"," <<k<<",\n";
+            }
+
     return 0;
 }
 
